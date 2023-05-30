b08b4a8a0659e4eb1818538d9777806960c32f81
deraui <cleoaumueller@gmail.com>
2023-05-30 15:55:27 +0200

    added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+      <ClerkProvider {...pageProps}>
+8      <Component {...pageProps} />
+9    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
